8ed94abd6407ae780ff870213cc9faeab92ac954
Bart <Bart0110@users.noreply.github.com>
2020-06-02 10:32:25 +0200

    Update wa-web-dark-toggle.user.js

--- wa-web-dark-toggle.user.js
+++ wa-web-dark-toggle.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         WhatsApp dark theme switcher enabler
-// @version      0.2
+// @version      0.2.1
 // @description  Adds WhatsApp dark theme switcher
 // @license      MIT
 // @author       Bart0110
@@ -57,7 +57,7 @@ SOFTWARE.`
 
     function addBtn() {
         const btnClass = document.querySelector('#side > header').lastElementChild.firstElementChild.firstElementChild.firstElementChild.attributes.class;
-        let switchBtn = '<div class="${btnClass}"><div role="button" id="wadark" title="Switch Dark/Light mode"><span data-icon="dark-switch" class><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="currentColor" d="M12,18V6A6,6 0 0,1 18,12A6,6 0 0,1 12,18M20,15.31L23.31,12L20,8.69V4H15.31L12,0.69L8.69,4H4V8.69L0.69,12L4,15.31V20H8.69L12,23.31L15.31,20H20V15.31Z" /></svg></span></div><span></span></div>';
+        let switchBtn = `<div class="${btnClass}"><div role="button" id="wadark" title="Switch Dark/Light mode"><span data-icon="dark-switch" class><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="currentColor" d="M12,18V6A6,6 0 0,1 18,12A6,6 0 0,1 12,18M20,15.31L23.31,12L20,8.69V4H15.31L12,0.69L8.69,4H4V8.69L0.69,12L4,15.31V20H8.69L12,23.31L15.31,20H20V15.31Z" /></svg></span></div><span></span></div>`;
 
         document.querySelector('#side > header').lastElementChild.firstElementChild.firstElementChild.insertAdjacentHTML('afterbegin', switchBtn);
 
